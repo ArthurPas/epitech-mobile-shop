69872a8bf41b938ac3f50084c19b14e6469d1bbd
Hugo Delattre <delattre.hugo.24@gmail.com>
2024-11-12 16:39:02 +0100

    feat: add project entities

--- src/order/entities/order.entity.ts
+++ src/order/entities/order.entity.ts
@@ -1,1 +1,40 @@
-export class Order {}
+import { BillingDetail } from 'src/billing-details/entities/billing-detail.entity';
+import { Orderline } from 'src/orderline/entities/orderline.entity';
+import { User } from 'src/user/entities/user.entity';
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  OneToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity()
+export class Order {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'decimal' })
+  total_price: number;
+
+  @Column()
+  creation_date: Date;
+
+  @Column()
+  payment_date: Date;
+
+  @Column()
+  is_paid: boolean;
+
+  @ManyToOne(() => User)
+  @JoinColumn({ name: 'user_id' })
+  user: User;
+
+  @ManyToOne(() => BillingDetail)
+  @JoinColumn({ name: 'billing_id' })
+  billing: BillingDetail;
+
+  @OneToMany(() => Orderline, (orderline) => orderline.order)
+  orderlines: Orderline[];
+}
--- src/product/entities/product.entity.ts
+++ src/product/entities/product.entity.ts
@@ -1,1 +1,21 @@
-export class Product {}
+import { Shop } from 'src/shop/entities/shop.entity';
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity()
+export class Product {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  open_food_fact_id: number;
+
+  @ManyToOne(() => Shop)
+  @JoinColumn({ name: 'shop_id' })
+  shop: Shop;
+}
