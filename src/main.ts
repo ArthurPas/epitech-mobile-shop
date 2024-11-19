005654713a46ed64078a0dcca77a208edc19fcc7
Hugo Delattre <delattre.hugo.24@gmail.com>
2024-11-19 17:07:51 +0100

    add Swagger

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Trinity')
+    .setDescription('Trinity API')
+    .setVersion('1.0')
+    .addTag('trinity')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
