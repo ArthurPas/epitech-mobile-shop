08c51266ae2c7add5f8eeb08eb79a0651b40a1dd
Hugo Delattre <delattre.hugo.24@gmail.com>
2024-12-22 21:59:59 +0100

    feat: add global pipe validation

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
   const allowedOrigins = ['http://localhost:3000', 'http://localhost:8080'];
   app.enableCors({
     origin: (origin, callback) => {
