e5f3078322dd3a89f9824c1d5d7da230014d38cf
Hugo Delattre <delattre.hugo.24@gmail.com>
2025-01-06 11:17:50 +0100

    feat: add swagger bearer config

--- src/main.ts
+++ src/main.ts
@@ -30,10 +30,25 @@ async function bootstrap() {
     .setDescription('Trinity API')
     .setVersion('1.0')
     .addTag('trinity')
-    .addBearerAuth()
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+        name: 'JWT',
+        description: 'Enter JWT token',
+        in: 'header',
+      },
+      'bearer',
+    )
+    .addSecurityRequirements('bearer')
     .build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, documentFactory);
+  SwaggerModule.setup('api', app, documentFactory, {
+    swaggerOptions: {
+      security: [{ bearer: [] }],
+    },
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
