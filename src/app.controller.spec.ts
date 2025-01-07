82973e4d5d2b29ece4769273bd1e91dbd27b7633
Hugo Delattre <delattre.hugo.24@gmail.com>
2025-01-07 12:11:21 +0100

    feat: add auth and inventory tests

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,14 +1,34 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthService } from './auth/auth.service';
+import { UserService } from './user/user.service';
 
 describe('AppController', () => {
   let appController: AppController;
 
+  const mockAuthService = {
+    validate: jest.fn(),
+  };
+
+  const mockUserService = {
+    findOne: jest.fn(),
+  };
+
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
       controllers: [AppController],
-      providers: [AppService],
+      providers: [
+        AppService,
+        {
+          provide: AuthService,
+          useValue: mockAuthService,
+        },
+        {
+          provide: UserService,
+          useValue: mockUserService,
+        },
+      ],
     }).compile();
 
     appController = app.get<AppController>(AppController);
