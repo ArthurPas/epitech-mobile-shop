674d710c9770096bf5fe193ce6d103b5cf4cb9fe
Hugo Delattre <hugo.delattre@epitech.eu>
2025-01-28 15:56:29 +0000

    feat: add paypal return urls

--- src/paypal/paypal.service.ts
+++ src/paypal/paypal.service.ts
@@ -6,14 +6,23 @@ import axios from 'axios';
 export class PaypalService {
   private readonly clientId: string;
   private readonly clientSecret: string;
-  private readonly baseUrl: string;
+  private readonly basePaypalUrl: string;
+  private readonly baseFrontUrl: string;
+  private readonly successPath: string;
+  private readonly cancelPath: string;
 
   constructor(private configService: ConfigService) {
     this.clientId = this.configService.get<string>('PAYPAL_CLIENT_ID');
     this.clientSecret = this.configService.get<string>('PAYPAL_CLIENT_SECRET');
-    this.baseUrl =
+    this.basePaypalUrl =
       this.configService.get<string>('PAYPAL_API') ||
       'https://api-m.sandbox.paypal.com';
+    this.baseFrontUrl =
+      this.configService.get<string>('FRONTEND_URL') || 'http://localhost:3000';
+    this.successPath =
+      this.configService.get<string>('PAYPAL_SUCCESS_PATH') || '/success';
+    this.cancelPath =
+      this.configService.get<string>('PAYPAL_CANCEL_PATH') || '/cancel';
 
     if (!this.clientId || !this.clientSecret) {
       throw new Error('PayPal credentials not found in environment variables');
@@ -26,7 +35,7 @@ export class PaypalService {
     );
 
     const response = await axios.post(
-      `${this.baseUrl}/v1/oauth2/token`,
+      `${this.basePaypalUrl}/v1/oauth2/token`,
       'grant_type=client_credentials',
       {
         headers: {
@@ -53,9 +62,13 @@ export class PaypalService {
     const accessToken = await this.getPaypalAccessToken();
 
     const response = await axios.post(
-      `${this.baseUrl}/v2/checkout/orders`,
+      `${this.basePaypalUrl}/v2/checkout/orders`,
       {
         intent: 'CAPTURE',
+        application_context: {
+          return_url: `${this.baseFrontUrl}${this.successPath}`,
+          cancel_url: `${this.baseFrontUrl}${this.cancelPath}`,
+        },
         purchase_units: [
           {
             amount: {
@@ -87,7 +100,7 @@ export class PaypalService {
     const accessToken = await this.getPaypalAccessToken();
 
     const response = await axios.post(
-      `${this.baseUrl}/v2/checkout/orders/${paypalOrderId}/capture`,
+      `${this.basePaypalUrl}/v2/checkout/orders/${paypalOrderId}/capture`,
       {},
       {
         headers: {
