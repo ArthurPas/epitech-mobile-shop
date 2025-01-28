ab1f25eacd1c03e169e7b615c1206964e346e525
louis lasserre <louis.lasserre@mmibordeaux.com>
2025-01-28 11:18:12 +0100

    fix crud product update

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -6,6 +6,7 @@ import {
   Param,
   Delete,
   Patch,
+  Query,
 } from '@nestjs/common';
 import { ProductService } from './product.service';
 import { CreateProductDto } from './dto/create-product.dto';
@@ -21,8 +22,8 @@ export class ProductController {
   }
 
   @Get()
-  findAll() {
-    return this.productService.findAll();
+  findAll(@Query('skip') skip: number, @Query('take') take: number) {
+    return this.productService.findAll(take, skip);
   }
 
   @Get(':id')
